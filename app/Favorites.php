851911057bf16a931b8fafc093ff87b416c720a6
sreymompnc <sreymom.mann@gmail.com>
2017-09-11 17:59:32 +0700

    add one more API

--- app/Favorites.php
+++ app/Favorites.php
@@ -6,7 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Favorites extends Model
 {
+    protected $table = 'favorites';
     protected $fillable = [
         'users_id', 'posts_id',
     ];
+
 }
